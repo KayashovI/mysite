e1d81d8493fa969b144eabdfdc867d7669d5842c
Kayashovl <Igorkashov@yandex.ru>
2024-03-19 09:40:22 +0300

    commit second

--- index.php
+++ index.php
@@ -15,6 +15,10 @@
         <tr>
             <td>
 
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla iste a perspiciatis facere quas distinctio magni eveniet, quasi ipsa quia, facilis cum. Fugit quaerat neque eum molestiae natus et suscipit!
+            </p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat voluptas ut quae ipsa deleniti commodi veritatis necessitatibus, possimus non a quam distinctio soluta repellat tenetur blanditiis dignissimos similique vitae iure?</p>
             </td>
         </tr>
         <tr>
