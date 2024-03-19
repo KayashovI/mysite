6530ec41bb6be7ef8541bacd920fb69a16f4a0bf
Kayashovl <Igorkashov@yandex.ru>
2024-03-19 09:53:24 +0300

    sloboda

--- about.php
+++ about.php
@@ -19,6 +19,71 @@
         </tr>
         <tr>
             <td>
+                ghsrthtrujhngws\thw\<table><h1>realpathtrs
+                    <h1>trh
+                        <tr><h1>stat
+                            <h1><tr><h1>tfrh
+                                <table>
+                                    realpathtrshs
+                                    gzfilehgf
+                                    namespacehgn
+
+                                    uik
+                                    ,l
+                                    <kbd>kl
+                                        <div class="kl.
+                                        .k
+                                        l.
+                                        kl.
+                                        kl
+                                        .
+                                        kl
+                                        .">
+                                    ukjhm
+                                base64_decodeb
+                            session_gc
+                        nj
+                    <tr>whilen
+                        endwtr
+                        namespace
+                        randtyn
+                        rtey
+
+                        yen
+
+
+                        wtyn42
+                        5y6
+
+                        y56bv
+                        6v
+                        jddayofweek
+                        rfjn
+                        <b><b>536
+
+                        yuvb3
+                        <b>6
+                            6bvy3
+
+                            bvhrte
+                            6
+                            wvy
+
+
+                            ywvby
+                            whilev
+                            wordwrapyw
+
+                            vy
+                        </b>
+                        </b></b>
+                    </tr></div>
+                                    </kbd>
+                                </table>
+                            </h1></tr></h1>
+                        </h1></tr>
+                    </h1>
+                </h1></table>
                 <!-- Подвал сайта: информация о правах на копирования, email разработчика -->
             </td>
         </tr>
